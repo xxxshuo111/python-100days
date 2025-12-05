7a302f057a4d4a035e51603797b23b4402c6b09e
li <shuoshuo.git@gmail.com>
2025-12-05 18:59:55 +0800

    更新day02代码

---
+++ day02.md
@@ -0,0 +1,3 @@
+1.for-in循环（有次数）
+2.while循环（无次数）：break和continue
+3.循环嵌套
---
+++ 循环求和.py
@@ -0,0 +1,23 @@
+#for循环：
+# total=0
+# for i in range(1,101):
+#     total+=i
+# print(total)
+#计算从1-100偶数求和
+# total=0
+# for i in range(0,101,2):
+#     total+=i
+#while循环：
+# total=0
+# i=1
+# while i<=100:
+#     total+=i
+#     i += 1
+# print(total)
+#计算计算从1-100偶数求和
+# total=0
+# i=0
+# while i<=100:
+#     total+=i
+#     i+=2
+# print(total)
